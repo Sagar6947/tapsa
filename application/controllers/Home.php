97dd74524142305b579d53f425d6cbf604447ddf
Sagar Thakur <sagarthakur6947@gmail.com>
2023-05-03 11:57:56 +0530

    ss

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,6 +4,7 @@ class Home extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('home');
+        $data['title'] = "TAPSA Electrohomeopathy Medical Institute";
+        $this->load->view('home', $data);
     }
 }
